efa6d6fdc83cb0d5f60f355943409a271773bb1b
juliusstr <julius@struwing.dk>
2023-04-26 11:18:56 +0200

    commit

---
+++ src/Test/TestTest.java
@@ -0,0 +1,26 @@
+package Test;
+
+import misc.ball.Ball;
+import misc.ball.PrimitiveBall;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.awt.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestTest {
+    Ball ball;
+
+    @BeforeEach
+    void setUp(){
+        ball = new Ball(0, 0, 4, new Color(1,1,1), true, PrimitiveBall.Status.UNKNOWN, -1, Ball.Type.UKNOWN);
+    }
+
+    @Test
+    @DisplayName("simpel ball test")
+    void simpelBallTest(){
+        assertEquals(1, ball.getPoint().x);
+    }
+}
