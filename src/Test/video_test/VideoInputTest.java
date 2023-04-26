a1cf7b5d09f3047bb07dc09be73285d0db5b9942
Ulleren <ulrikbp@gmail.com>
2023-04-26 13:25:54 +0200

    added test videos

---
+++ src/Test/video_test/VideoInputTest.java
@@ -0,0 +1,16 @@
+package Test.video_test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.opencv.videoio.VideoCapture;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VideoInputTest {
+    VideoCapture capture = new VideoCapture("/home/")
+    @Test
+    @DisplayName("simpel ball test")
+    void videoInputTest(){
+
+    }
+}
