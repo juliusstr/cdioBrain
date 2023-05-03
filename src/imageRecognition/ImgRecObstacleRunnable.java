c8445f1b46ee9f406b05f076f48436bf27493675
juliusstr <julius@struwing.dk>
2023-05-03 14:54:43 +0200

    ImgRecObstacle moved

--- src/imageRecognition/ImgRecObstacleRunnable.java
+++ src/imageRecognition/ImgRecObstacleRunnable.java
@@ -1,36 +1,21 @@
-package Temp;
+package imageRecognition;
 
 import exceptions.BadDataException;
 import imageRecognition.ImgRecObstacle;
-import misc.Vector2Dv1;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
-import org.opencv.videoio.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.highgui.HighGui;
-import org.opencv.core.MatOfPoint3;
 import org.opencv.core.Scalar;
 import org.opencv.imgcodecs.Imgcodecs;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.CvType;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.imgproc.Moments;
-import org.opencv.core.Rect;
-import org.opencv.core.RotatedRect;
-import org.opencv.core.TermCriteria;
-import org.opencv.imgproc.Subdiv2D;
 
 import java.awt.*;
-import java.io.*;
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Array;
 import java.util.ArrayList;
-import java.util.List;
-public class main {
+
+public class ImgRecObstacleRunnable {
     public static void main(String[] args) throws IOException, BadDataException {
         System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
         Mat input = Imgcodecs.imread("test_img/WIN_20230315_10_32_53_Pro.jpg");
