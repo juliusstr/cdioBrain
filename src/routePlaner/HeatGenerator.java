f7773dba6f6ad4254904fcf887c4937610c31b88
Chris0Rask <104867759+Chris0Rask@users.noreply.github.com>
2023-06-10 12:52:17 +0200

    Heatgen class

---
+++ src/routePlaner/HeatGenerator.java
@@ -0,0 +1,33 @@
+package routePlaner;
+
+import misc.Vector2Dv1;
+import misc.ball.Ball;
+
+import java.util.ArrayList;
+
+public class HeatGenerator {
+
+    public void orangeRoute() {
+
+        ArrayList<Route> best_route = new ArrayList<>();
+
+
+
+        int best_score;
+        int i = 0;
+        int j = 0;
+
+        for (i = 0; i < ball_list.size(); i++){
+            if(ball_list.get(i).Route.score<best_score){
+                best_route.get(j) = ball_list.get(i).Route;
+                j++;
+            }
+
+        }
+
+
+
+
+    }
+
+}
