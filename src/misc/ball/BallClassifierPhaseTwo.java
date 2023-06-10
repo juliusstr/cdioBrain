8dbe33f246b46127566d25d9ea214e31b7b7af08
juliusstr <julius@struwing.dk>
2023-06-10 12:12:47 +0200

    temp

--- src/misc/ball/BallClassifierPhaseTwo.java
+++ src/misc/ball/BallClassifierPhaseTwo.java
@@ -1,6 +1,12 @@
 package misc.ball;
+import Client.StandardSettings;
+import misc.*;
+import nav.NavAlgoPhaseTwo;
+
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
+
 
 public class BallClassifierPhaseTwo {
 
@@ -53,4 +59,30 @@ public class BallClassifierPhaseTwo {
         }
         return closestColer;
     }
+    
+    public static void ballSetPlacement(Ball ball, Boundry boundry, Cross cross){
+
+        ArrayList<Zone> zones = cross.getCriticalZones();
+        for (Zone zone: zones) {
+            if(zone.posInsideZone(ball.getPosVector())){
+                ball.setPlacement(Ball.Placement.CORNER);
+                break;
+            }
+        }
+        if(ball.getPlacement() != Ball.Placement.CORNER){
+            int edgeCloseenughCount = 0;
+            ArrayList<LineDist> lineDists = new ArrayList<>();
+            for (Line line : boundry.bound) {
+                lineDists.add(new LineDist(line, line.findDistanceToPoint(ball.getPosVector())));
+                if(lineDists.get(lineDists.size()-1).distToline < StandardSettings.BALL_RADIUS_PX + Zone.CRITICAL_ZONE_RADIUS) {
+                    edgeCloseenughCount++;
+                }
+            }
+        }
+
+        
+        //todo finde out if its in a corner or a edge
+    }
+    
+
 }
