3d34bc9e8d3a761eed83e716e8095ff67dd4f70e
jj <jj@hovmark.dk>
2023-06-10 17:06:17 +0200

    route to waypoints

--- src/routePlaner/Route.java
+++ src/routePlaner/Route.java
@@ -10,20 +10,20 @@ import java.util.List;
 public class Route {
     private Ball end = null;
     private Vector2Dv1 startPoint = null;
-    private List<Vector2Dv1> route = null;
+    private List<Vector2Dv1> waypoints = null;
     private int score = 0;
 
     public Route(Vector2Dv1 v){ startPoint = v; }
     public void setScore(int s){ score = s; }
-    public void setRoute(List<Vector2Dv1> r){ route = r; }
+    public void setWaypoints(List<Vector2Dv1> r){ waypoints = r; }
     public void setEnd(Ball e){ end = e; }
 
     public int getScore(){
         return score;
     }
 
-    public List<Vector2Dv1> getRoute(){
-        return route;
+    public List<Vector2Dv1> getWaypoints(){
+        return waypoints;
     }
 
     public Ball getEnd(){
